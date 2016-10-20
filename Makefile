a4399f20bae84cabf55bc9785b5dab34d60c5b14
Kieran Kelk <kak3@st-andrews.ac.uk>
2016-10-20 15:23:40 +0100

    Implement function classes
    These do not yet support arguments.

---
+++ Makefile
@@ -0,0 +1,3 @@
+bar.class: bar.j ast.py
+	python3 ast.py
+	java -jar ~/jasmin-2.4/jasmin.jar *.j
--- ast.py
+++ ast.py
@@ -85,8 +85,22 @@ class ASTNode():
 
         return ret
 
-    def generate_code(self):
+    def _emit(self):
         raise NotImplementedError
+
+    def _emit_target(self):
+        """By default, emit to a return string. Can be overriden in subclasses to return a (non-empty) string indicating the
+        file name to write to. In this case, the empty string will be returned from emit()."""
+        return ''
+
+    def emit(self):
+        """Emits the result of self._emit() to the self._emit_target()."""
+        if self._emit_target():
+            with open(self._emit_target(), 'w') as outfile:
+                outfile.write(self._emit())
+            return ''
+        else:
+            return self._emit()
 
 class Terminal(ASTNode):
     make_fn = NotImplemented
@@ -103,16 +117,16 @@ class Terminal(ASTNode):
 class Expr(ASTNode):
     pass
 
-class Decl(ASTNode):
+class Declaration(ASTNode):
     pass
 
-class Decls(ASTNode):
+class Declarations(ASTNode):
     required_children = {
-        'decls': (GreaterOrEqual(0), Decl)
+        'decls': (GreaterOrEqual(0), Declaration)
     }
 
-    def generate_code(self):
-        return '\n'.join(map(lambda x: x.generate_code(), self.decls))
+    def _emit(self):
+        return '\n'.join(map(lambda x: x.emit(), self.decls))
 
 class Type(ASTNode):
     pass
@@ -125,12 +139,17 @@ class Identifier(Expr, Terminal):
 
         self._value = value
 
-    def generate_code(self):
-        return """iload_0"""
+    def _emit(self):
+        return """
+        new {name}Function
+        dup
+        invokespecial {name}Function.<init>()V
+        invokevirtual {name}Function.apply()I
+        """.format(name=self.value)
 
 class Int(Expr, Terminal):
     make_fn = int
-    def generate_code(self):
+    def _emit(self):
         return 'bipush {}'.format(self.value)
 
 class Double(Expr, Terminal):
@@ -195,15 +214,19 @@ class Module(ASTNode):
     required_children = {
         'id': (Exactly(1), Identifier),
         'expr': (Exactly(1), Expr),
-        'decls': (GreaterOrEqual(0), Decls)
+        'decls': (GreaterOrEqual(0), Declarations)
     }
 
-    def generate_code(self):
+    def _emit_target(self):
+        return '{}.j'.format(self.id.value)
+
+    def _emit(self):
         return '''
         .class public {name}
         .super java/lang/Object
 
         .method public static module()I
+            .limit stack 2
             {decls}
             {expr}
             ireturn
@@ -216,27 +239,44 @@ class Module(ASTNode):
             invokevirtual java/io/PrintStream/println(I)V
             return
         .end method
-        '''.format(name=self.id.value, decls=self.decls.generate_code(), expr=self.expr.generate_code())
+        '''.format(name=self.id.value, decls=self.decls.emit(), expr=self.expr.emit())
 
-class FuncDecl(Decl):
+class FunctionDeclaration(Declaration):
     required_children = {
-        'id': (GreaterOrEqual(1), Identifier),
+        'id': (Exactly(1), Identifier),
+        'params': (GreaterOrEqual(0), Identifier),
         'expr': (Exactly(1), Expr)
     }
 
-    def generate_code(self):
+    def _emit(self):
         return """
-        {expr}
-        istore_0
-        """.format(expr=self.expr.generate_code())
+        .class public {name}Function
+        .super java/lang/Object
 
-class TaskFarm(Decl):
+        .method public <init>()V
+            aload_0
+
+            invokenonvirtual java/lang/Object/<init>()V
+            return
+        .end method
+
+        .method public apply()I
+            {expr}
+            ireturn
+        .end method
+        """.format(name=self.id.value, expr=self.expr.emit())
+
+    def _emit_target(self):
+        return '{}Function.j'.format(self.id.value)
+
+
+class DataTypeDeclaration(Declaration):
     required_children = {
         'id': (Exactly(1), Identifier),
         'constrs': (Exactly(1), Constrs)
     }
 
-class TypeDecl(Decl):
+class TypeDeclaration(Declaration):
     required_children = {
         'id': (Exactly(1), Identifier),
         'type': (Exactly(1), Type)
@@ -260,5 +300,5 @@ class FunctionType(Type):
 
 if __name__ == '__main__':
     #module = Module(id=Identifier("foo"), expr=Int(42)) # module foo = 42
-    module = Module(id=Identifier("bar"), expr=Identifier("x"), decls=Decls(decls=[FuncDecl(id=Identifier("x"), expr=Int(3))])) # module bar = x where { x = 3 }
-    print(module.generate_code())
+    module = Module(id=Identifier("bar"), expr=Identifier("x"), decls=Declarations(decls=[FunctionDeclaration(id=Identifier("x"), expr=Int(3))])) # module bar = x where { x = 3 }
+    module.emit()
