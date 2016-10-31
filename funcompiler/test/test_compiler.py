59ee093b654188486e272d194d13078dbb52cb8a
Kieran Kelk <kak3@st-andrews.ac.uk>
2016-10-31 13:18:55 +0000

    Clean up test formatting.

--- funcompiler/test/test_compiler.py
+++ funcompiler/test/test_compiler.py
@@ -28,61 +28,167 @@ def check(module, expected_output, scope=None):
         raise
 
 def test_int():
-    module = Module(id=Identifier("inttest"), expr=Int(42)) # module foo = 42
+    # module IntTest = 42
+    module = Module(
+            id=Identifier("IntTest"),
+            expr=Int(42)
+    )
+    
     check(module, 42)
+
+def test_double():
+    # module DoubleTest = 36.0
+    module = Module(
+            id=Identifier("DoubleTest"),
+            expr=Double(36)
+    )
+
+    check(module, 36.0)
+
+def test_char():
+    scope = Scope()
+
+    # module CharTest = 'a'
+    module = Module(
+            id=Identifier("CharTest"),
+            expr=Char("a")
+    )
+    
+    check(module, 'a', scope)
 
 def test_zero_args():
     scope = Scope()
 
-    module = Module(id=Identifier("zeroargstest"), expr=Identifier("x"), decls=[FunctionDeclaration(scope, id=Identifier("x"), expr=Int(3))]) # module bar = x where { x = 3 }
+    # module ZeroArgTest = x where { x = 3 }
+    module = Module(
+            id=Identifier("ZeroArgTest"),
+            expr=Identifier("x"),
+            decls=[FunctionDeclaration(
+                scope,
+                id=Identifier("x"),
+                expr=Int(3)
+            )]
+    )
+
     check(module, 3, scope)
 
 def test_ord():
-    module = Module(id=Identifier("ordtest"), expr=UnaryOperator(expr=Char("a"), op=Operator("ord")))
+    # module OrdTest = ord('a')
+    module = Module(
+            id=Identifier("OrdTest"),
+            expr=UnaryOperator(
+                expr=Char("a"),
+                op=Operator("ord")
+            )
+    )
 
     check(module, 97)
 
-def test_chr():
-    module = Module(id=Identifier("chrtest"), expr=UnaryOperator(expr=Int(97), op=Operator("chr")))
+def test_chr_op():
+    # module ChrOpTest = chr(97)
+    module = Module(
+            id=Identifier("ChrOpTest"),
+            expr=UnaryOperator(
+                expr=Int(97),
+                op=Operator("chr")
+            )
+    )
 
     check(module, "a")
 
-def test_character():
+def test_char_function():
     scope = Scope()
 
-    module = Module(id=Identifier("chartest"), expr=Char("a"))
-    check(module, 'a', scope)
+    # module CharFuncTest = c where { c = 'c' }
+    module = Module(
+            id=Identifier("CharFuncTest"),
+            expr=Identifier("c"),
+            decls=[FunctionDeclaration(
+                scope,
+                id=Identifier("c"),
+                expr=Char("c")
+            )]
+    )
 
-def test_character_function():
-    scope = Scope()
-
-    module = Module(id=Identifier("charfunctest"), expr=Identifier("c"), decls=[FunctionDeclaration(scope, id=Identifier("c"), expr=Char("c"))])
+    check(module, "c")
 
 def test_int_operators():
     scope = Scope()
 
-    module = Module(id=Identifier("intoperatorstest"), expr=BinaryOperator(expr1=Identifier("x"), op=Operator("+"), expr2=Int(1)), decls=[FunctionDeclaration(scope, id=Identifier("x"), expr=BinaryOperator(expr1=Int(3), op=Operator("*"), expr2=Int(2)))]) # module bar = x + 1 where { x = 3 * 2 }
+    # module IntOperatorsTest = x + 1 where { x = 3 * 2 }
+    module = Module(
+            id=Identifier("IntOperatorsTest"),
+            expr=BinaryOperator(
+                expr1=Identifier("x"),
+                op=Operator("+"),
+                expr2=Int(1)
+            ),
+            decls=[FunctionDeclaration(
+                scope,
+                id=Identifier("x"),
+                expr=BinaryOperator(
+                    expr1=Int(3),
+                    op=Operator("*"),
+                    expr2=Int(2)
+                )
+            )]
+    ) 
     check(module, 7, scope)
 
 def test_double_operators():
     scope = Scope()
 
-    module = Module(id=Identifier("doubleoperatorstest"), expr=BinaryOperator(expr1=Identifier("x"), op=Operator("+"), expr2=Double(1)), decls=[FunctionDeclaration(scope, id=Identifier("x"), expr=BinaryOperator(expr1=Double(3), op=Operator("*"), expr2=Double(2)))]) # module bar = x + 1 where { x = 3 * 2 }
+    # module DoubleOperatorsTest = x + 1 where { x = 3 * 2 }
+    module = Module(
+            id=Identifier("DoubleOperatorsTest"),
+            expr=BinaryOperator(
+                expr1=Identifier("x"),
+                op=Operator("+"),
+                expr2=Double(1)
+            ),
+            decls=[FunctionDeclaration(
+                scope,
+                id=Identifier("x"),
+                expr=BinaryOperator(
+                    expr1=Double(3),
+                    op=Operator("*"),
+                    expr2=Double(2)
+                )
+            )]
+    )
+    
     check(module, 7.0, scope)
 
 def test_one_arg():
     scope = Scope()
 
-    module = Module(id=Identifier("oneargtest"), expr=FunctionApplication(func=Identifier("f"), expr=Int(2)), decls=[FunctionDeclaration(scope, id=Identifier("f"), params=[Identifier("x")], expr=BinaryOperator(expr1=Identifier("x"), op=Operator("*"), expr2=Int(3)))]) # module bar = f 2 where { f x = x * 3 }
+    # module OneArgTest = f 2 where { f x = x * 3 }
+    module = Module(
+            id=Identifier("OneArgTest"),
+            expr=FunctionApplication(
+                func=Identifier("f"),
+                expr=Int(2)
+            ),
+            decls=[FunctionDeclaration(
+                scope,
+                id=Identifier("f"),
+                params=[Identifier("x")],
+                expr=BinaryOperator(
+                    expr1=Identifier("x"),
+                    op=Operator("*"),
+                    expr2=Int(3)
+                )
+            )]
+    ) 
 
     check(module, 6, scope)
 
 def test_two_args():
     scope = Scope()
 
-    # module bar = mul 2 5 where { mul x y = x * y }
+    # module TwoArgTest = mul 2 5 where { mul x y = x * y }
     module = Module(
-            id=Identifier("twoargstest"),
+            id=Identifier("TwoArgTest"),
             expr=FunctionApplication(
                 func=FunctionApplication(
                     func=Identifier("mul"),
